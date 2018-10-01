3c563d759088871ddb82e9ffc7273dec4efff036
TimNNyman <tim.n.nyman@gmail.com>
2018-10-01 08:27:41 +0200

    MOved all file reading over to one class

--- WebApp_Assignment2/default.aspx.cs
+++ WebApp_Assignment2/default.aspx.cs
@@ -22,18 +22,9 @@ namespace WebApp_Assignment2
 
         private string getNewsAsHtmlString()
         {
-            List<newsData> news = new List<newsData>();
-            using (StreamReader r = new StreamReader(Server.MapPath("~/ ") + "/json/news.json"))
-            {
-                string json = r.ReadToEnd();
-                news = JsonConvert.DeserializeObject<List<newsData>>(json);
-            }
-            List<newsData> uNews = new List<newsData>();
-            using (StreamReader r = new StreamReader(Server.MapPath("~/ ") + "/json/updatableNews.json"))
-            {
-                string json = r.ReadToEnd();
-                uNews = JsonConvert.DeserializeObject<List<newsData>>(json);
-            }
+            List<newsData> news = DatabaseConnector.Inst.readNewsData(Server.MapPath("~/ ") + "/json/news.json");
+            List<newsData> uNews = DatabaseConnector.Inst.readNewsData(Server.MapPath("~/ ") + "/json/updatableNews.json");
+
             string retString = "";
 
             for (int i = 0; i < uNews.Count; i++)
